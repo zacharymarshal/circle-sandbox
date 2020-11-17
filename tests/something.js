13362c3f50a7fecda8d7326f99be5ac620cb3507
Zachary Rankin <zach@rknmail.com>
2020-11-17 08:19:31 +0000

    Add another test

--- tests/something.js
+++ tests/something.js
@@ -4,3 +4,8 @@ test("gotta test something", function (t) {
   t.plan(1);
   t.equal(typeof Date.now, "function");
 });
+
+test("testing something else", function (t) {
+  t.plan(1);
+  t.equal(typeof "hello", "string");
+});
